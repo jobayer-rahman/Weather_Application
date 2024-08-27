5c90bdc7b1bb5ab38666a28a323bb44e2af62e2b
Jobayer Rahman <jobayer_rahman@yahoo.com>
2024-08-28 02:24:28 +0600

    Initial commit for backendsvc and weather_api

---
+++ backend/weathersvc/weathersvc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for weathersvc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weathersvc.settings')
+
+application = get_asgi_application()
